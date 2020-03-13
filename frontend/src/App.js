14181a34e3b09b8d991e8ad91bea92d8a5d87290
David Ngassa <ngassadavid97@gmail.com>
2020-03-13 20:54:00 +0400

    Set token auto logout and timeout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,38 +15,67 @@ import MainNavigation from "./shared/Navigation/MainNavigation";
 import Forecast from "./cities/pages/Forecast";
 import "./App.css";
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
+
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); // Generates a date which is current date plus 1hr
+
+    setTokenExpirationDate(tokenExpirationDate);
 
     // Store user data in local storage
     localStorage.setItem(
       "userData",
       JSON.stringify({
         userId: uid,
-        token: token
+        token: token,
+        expiration: tokenExpirationDate.toISOString()
       })
     );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
 
     // Clear data
     localStorage.removeItem("userData");
   }, []);
 
+  // Set Timer for token expiration
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
   // Configure auto-login
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
 
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
--- frontend/src/shared/Navigation/NavLinks.js
+++ frontend/src/shared/Navigation/NavLinks.js
@@ -7,10 +7,18 @@ import { FiSearch } from "react-icons/fi";
 const NavLinks = props => {
   const auth = useContext(AuthContext);
 
+  const logoutHandler = event => {
+    auth.logout();
+  };
+
   return (
     <div className="header-right">
       {auth.isLoggedIn && <NavLink to="/home">Home</NavLink>}
-      {auth.isLoggedIn && <NavLink to="/auth">Logout</NavLink>}
+      {auth.isLoggedIn && (
+        <NavLink to="/" onClick={logoutHandler}>
+          Logout
+        </NavLink>
+      )}
       {auth.isLoggedIn && (
         <NavLink to="/search">
           <FiSearch />
