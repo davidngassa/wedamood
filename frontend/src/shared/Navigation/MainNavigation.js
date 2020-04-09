70b66d6412c21c34a614f772178155c7f1b45032
David Ngassa <ngassadavid97@gmail.com>
2020-04-10 00:08:13 +0400

    mobile header drawer

--- frontend/src/shared/Navigation/MainNavigation.js
+++ frontend/src/shared/Navigation/MainNavigation.js
@@ -1,21 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { AuthContext } from "../context/auth-context";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
+import HamburgerIcon from "../../images/hamburger-icon.png";
 
-const MainNavigation = props => {
+const MainNavigation = (props) => {
   const auth = useContext(AuthContext);
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
 
   return (
-    <MainHeader>
-      <Link className="logo" to={auth.isLoggedIn ? "/home" : "/"}>
-        wedamood
-      </Link>
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
 
-      <NavLinks />
-    </MainHeader>
+      <SideDrawer
+        show={drawerIsOpen && auth.isLoggedIn}
+        onClick={closeDrawerHandler}
+      >
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+        {auth.isLoggedIn && (
+          <div id="drawer-button" onClick={openDrawerHandler}>
+            <img src={HamburgerIcon} alt="menu-icon" />
+          </div>
+        )}
+
+        <Link className="logo" to={auth.isLoggedIn ? "/home" : "/"}>
+          wedamood
+        </Link>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
